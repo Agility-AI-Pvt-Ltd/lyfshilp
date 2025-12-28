e5f3a9f0de9c48157b6ee978c4a37e45740f83fd
AnujYelve <anujyelve3074@gmail.com>
2025-12-28 11:06:44 +0530

    Fix import paths for images and components in Thirdsection and Olympiad pages

--- lyfshilp-frontend/src/pages/Olympiad.jsx
+++ lyfshilp-frontend/src/pages/Olympiad.jsx
@@ -3,7 +3,7 @@ import OlympiadSection1 from "../ComponentOlympiad/OlympiadSection1.jsx";
 import OlympiadSection2 from "../ComponentOlympiad/OlympiadSection2.jsx";
 import OlympiadSection3 from "../ComponentOlympiad/OlympiadSection3.jsx";
 import OlympiadSection4 from "../ComponentOlympiad/OlympiadSection4.jsx";
-import OlympiadSection5 from "../ComponentOlympiad/olympiadSection5.jsx";
+import OlympiadSection5 from "../ComponentOlympiad/OlympiadSection5.jsx";
 import OlympiadSection7 from "../ComponentOlympiad/OlympiadSection7.jsx";
 import JoinLearnSection from "../ComponentOlympiad/JoinLearnSection.jsx";
 import OlympiadDash from "../ComponentOlympiad/OlymiadDash.jsx";
